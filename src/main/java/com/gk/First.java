0ab056b6d2a13ccf6e999587171aa478266075ef
satya gopal <satyagopal1233@gmail.com>
2021-06-26 21:29:00 +0530

    updated First.java

--- src/main/java/com/gk/First.java
+++ src/main/java/com/gk/First.java
@@ -3,7 +3,10 @@ package com.gk;
 First Application
  */
 public class First {
-    public static void main(final String[] args){
+    /*
+    main memthod
+ */
+    public static void main(final String[] args) {
         System.out.println("My First application is First App");
         System.out.println("My Second printout stmt");
         System.out.println("My Third Printout stmt");
