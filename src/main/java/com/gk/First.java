8c6b75fac9213569a2ed3448eb89aea44fcfda39
satya gopal <satyagopal1233@gmail.com>
2021-06-26 21:25:24 +0530

    updated First.java

--- src/main/java/com/gk/First.java
+++ src/main/java/com/gk/First.java
@@ -1,6 +1,9 @@
 package com.gk;
+/*
+First Application
+ */
 public class First {
-    public static void main(String args[]){
+    public static void main(final String[] args){
         System.out.println("My First application is First App");
         System.out.println("My Second printout stmt");
         System.out.println("My Third Printout stmt");
