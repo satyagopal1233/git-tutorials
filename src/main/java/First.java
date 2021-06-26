46b2defbf074659534fa25329195ba7ffa3b1e48
satya gopal <satyagopal1233@gmail.com>
2021-06-26 20:10:33 +0530

    added another sysout in First.java

--- src/main/java/First.java
+++ src/main/java/First.java
@@ -1,5 +1,6 @@
 public class First {
     public static void main(String args[]){
         System.out.println("My First application");
+        System.out.println("My Second printout stmt");
     }
 }
