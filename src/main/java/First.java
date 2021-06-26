456295847e78e12fb2f754b1596a9b42c0eb270e
satya gopal <satyagopal1233@gmail.com>
2021-06-26 20:12:16 +0530

    changed first sysout stmt

--- src/main/java/First.java
+++ src/main/java/First.java
@@ -1,6 +1,6 @@
 public class First {
     public static void main(String args[]){
-        System.out.println("My First application");
+        System.out.println("My First application is First App");
         System.out.println("My Second printout stmt");
     }
 }
